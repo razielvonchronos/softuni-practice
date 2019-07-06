2acecee4522734f2aff48c06bc00dcf3a7906bc0
razielvonchronos <razielvonchronos@gmail.com>
2019-07-06 07:13:10 +0300

    update Last Digit Name.php

--- PHPWebBacis/Exercise/Defining Classes/09. Last Digit Name.php
+++ PHPWebBacis/Exercise/Defining Classes/09. Last Digit Name.php
@@ -1,3 +1,51 @@
 <?php
 
 
+class Number
+{
+    /**
+     * @var int[] $number
+     */
+    private $number;
+    /**
+     * @var array $dictionary
+     */
+    private $dictionary = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+    /**
+     * @var array $digits
+     */
+    private $digits = [];
+
+    /**
+     * Number constructor.
+     * @param int $number
+     */
+    public function __construct(int $number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @param int $i
+     * @return int
+     */
+    public function IntToString()
+    {
+        $number = end($this->digits);
+        return $this->dictionary[$number];
+    }
+
+    /**
+     * @return $this
+     */
+    public function ExplodeNumber()
+    {
+        $this->digits = str_split($this->number);
+        return $this;
+    }
+}
+
+$input = intval(readline());
+$number = new Number($input);
+printf('%s', $number->ExplodeNumber()->IntToString());
