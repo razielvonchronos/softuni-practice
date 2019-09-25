85ce6c1d0ff12969e9e8c321cdff52a976daf156
razielvonchronos <razielvonchronos@gmail.com>
2019-09-25 23:13:47 +0300

    Updated CookingByNumbers for better input handling

--- JS Advanced/01. JS-Advanced-Syntax-Functions-and-Statements/07. Cooking by Numbers.js
+++ JS Advanced/01. JS-Advanced-Syntax-Functions-and-Statements/07. Cooking by Numbers.js
@@ -1,15 +1,14 @@
-function solve(cmd) {
-    let incredient = Number(cmd.shift());
+function solve(input) {
+    let [incredient, cmd] = [Number(input[0]), input.slice(1)];
     let map = {
-        chop(x){ return x / 2 },
-        dice(x){ return Math.sqrt(x) },
-        spice(x){ return x + 1 },
-        bake(x){ return x * 3 },
-        fillet(x){ return x -= x * 0.2 }
+        chop(x) { return x / 2 },
+        dice(x) { return Math.sqrt(x) },
+        spice(x) { return x + 1 },
+        bake(x) { return x * 3 },
+        fillet(x) { return x -= x * 0.2 }
     }
 
-    for(let c of cmd)
-    {
+    for (let c of cmd) {
         incredient = map[c](incredient);
         console.log(incredient);
     }
