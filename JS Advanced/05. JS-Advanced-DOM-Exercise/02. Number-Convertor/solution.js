baf710bb704c59f21736ee28f42b528b378dd5ce
razielvonchronos <razielvonchronos@gmail.com>
2019-10-21 20:13:26 +0300

    Removed menu options from the HTML and added them trough solution.js

--- JS Advanced/05. JS-Advanced-DOM-Exercise/02. Number-Convertor/solution.js
+++ JS Advanced/05. JS-Advanced-DOM-Exercise/02. Number-Convertor/solution.js
@@ -1,4 +1,16 @@
 function solve() {
+
+    let to = document.querySelector('#selectMenuTo');
+    let option_binary = document.createElement('option');
+    let option_hexadecimal = document.createElement('option');
+
+    option_binary.text = "Binary"
+    option_binary.value = "binary"
+    option_hexadecimal.text = "Hexadeicmal"
+    option_hexadecimal.value = "hexadecimal"
+    to.appendChild(option_binary);
+    to.appendChild(option_hexadecimal);
+
     let HandleConvertion = (e) => {
         let convert = {
             binary(dec) {
@@ -7,13 +19,11 @@ function solve() {
             },
             hexadecimal: (dec) => {
                 const result = dec.toString(16).toUpperCase();
-                console.log(`${dec} to ${result}`)
                 return result;
             },
         }
 
         let input = document.querySelector('#input');
-        let to = document.querySelector('#selectMenuTo');
 
         let result = document.querySelector('#result');
         result.value = convert[to.value](Number(input.value));
