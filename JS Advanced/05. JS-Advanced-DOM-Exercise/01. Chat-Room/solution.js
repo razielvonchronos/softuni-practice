ae60cb3fd6acf99980a83467ca7713363464c82c
razielvonchronos <razielvonchronos@gmail.com>
2019-10-21 18:59:02 +0300

    Chat-Room and Number-Converter are ready.

--- JS Advanced/05. JS-Advanced-DOM-Exercise/01. Chat-Room/solution.js
+++ JS Advanced/05. JS-Advanced-DOM-Exercise/01. Chat-Room/solution.js
@@ -1,5 +1,14 @@
 function solve() {
-   //TODO...
+   function HandleButtonSend(e) {
+      let input = document.querySelector('#chat_input');
+      let output = document.createElement('div');
+      output.classList.add('message', 'my-message');
+      output.innerHTML = input.value;
+      console.log(output)
+
+      let chat_messages = document.querySelector('#chat_messages');
+      chat_messages.appendChild(output);
+      input.value = '';
+   }
+   document.getElementById('send').addEventListener('click', HandleButtonSend);
 }
-
-
--- JS Advanced/05. JS-Advanced-DOM-Exercise/02. Number-Convertor/solution.js
+++ JS Advanced/05. JS-Advanced-DOM-Exercise/02. Number-Convertor/solution.js
@@ -1,4 +1,22 @@
 function solve() {
+    let HandleConvertion = (e) => {
+        let convert = {
+            binary(dec) {
+                const result = (dec >>> 0).toString(2);
+                return result;
+            },
+            hexadecimal: (dec) => {
+                const result = dec.toString(16).toUpperCase();
+                console.log(`${dec} to ${result}`)
+                return result;
+            },
+        }
 
-    //TODO...
+        let input = document.querySelector('#input');
+        let to = document.querySelector('#selectMenuTo');
+
+        let result = document.querySelector('#result');
+        result.value = convert[to.value](Number(input.value));
+    }
+    document.querySelector('button').addEventListener('click', HandleConvertion)
 }
