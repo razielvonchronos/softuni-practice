4b5c15b3534deac9b8ca4f686bba26c01e1aeee6
razielvonchronos <razielvonchronos@gmail.com>
2019-10-25 00:57:23 +0300

    Finished 02. PizzUni

--- JS Advanced/Exam preparation/Exam - 07 July 2019/02. PizzUni/test/index.test.js
+++ JS Advanced/Exam preparation/Exam - 07 July 2019/02. PizzUni/test/index.test.js
@@ -4,26 +4,20 @@ const should = require('chai').should;
 const beforeEach = require('mocha').beforeEach;
 const PizzUni = require('../index.js');
 
-
-
-
 describe("Tests ...", function () {
-  /** class PizzUni */
   let PizzaShop;
-  let users;
   let products;
   let orders;
   let validUser;
+  let ProductsDefaults;
 
   beforeEach(() => {
     PizzaShop = new PizzUni();
     users = PizzaShop.registeredUsers;
     products = PizzaShop.availableProducts;
     orders = PizzaShop.orders;
-  });
-
-  describe('contructor tests', () => {
-    let ProductsDefaults = {
+    validUser = 'raziel@abv.bg';
+    ProductsDefaults = {
       "drinks": [
         "Coca-Cola",
         "Fanta",
@@ -35,86 +29,90 @@ describe("Tests ...", function () {
         "Classic Margherita",
       ]
     }
+  });
 
+  describe('contructor tests', () => {
     it('should initialize with properties correctly', () => {
-      expect(users).to.deep.equal([]);
-      expect(products).to.deep.equal(ProductsDefaults);
-      expect(orders).to.deep.equal([]);
+      let expectedKeys = { registeredUsers: [], availableProducts: ProductsDefaults, orders: [] };
+      assert.deepOwnInclude(PizzaShop, expectedKeys);
     })
   })
 
   describe("registerUser() tests", function () {
-
-    // it('should accept only strings', () => {
-    //   let input = {};
-    //   let result = PizzaShop.registerUser(input);
-    //   expect(typeof result.email).to.be.equal('string')
-    // })
-
-    it('should throw error on dublicate', function () {
-      validUser = 'raziel@abv.bg';
-      PizzaShop.registerUser(validUser);
-      let actual = () => PizzaShop.registerUser(validUser);
-      expect(actual).to.throw(Error, `This email address (${validUser}) is already being used!`);
+    it('Receives a string (email)', () => assert.isString(PizzaShop.registerUser(validUser).email))
+    it('the email is already used', () => {
+      let user = PizzaShop.registerUser(validUser);
+      let action = () => PizzaShop.registerUser(user.email);
+      assert.throw(action, Error, `This email address (${user.email}) is already being used!`);
     })
 
-    it('should return object of user', function () {
-      let expected = {
-        email: validUser,
-        orderHistory: []
-      }
-      let actual = PizzaShop.registerUser(validUser);
-      expect(actual).to.deep.equal(expected);
+    it("upon registration, registeredUsers hold user's email and orderHistory", () => {
+      let user = PizzaShop.registerUser(validUser);
+      assert.deepEqual(PizzaShop.doesTheUserExist(user.email), { email: user.email, orderHistory: [] })
+    })
+
+    it('returns object with given email and orderHistory which is an empty array.', () => {
+      let user = PizzaShop.registerUser(validUser);
+      assert.deepEqual(user, { email: user.email, orderHistory: [] })
     })
   })
 
   describe("makeAnOrder() tests", function () {
     it('Should throw error on wrong email', () => {
-      let expectedWongEmail = () => PizzaShop.makeAnOrder('pesho@abv.bg', products.pizzas[0], products.drinks[0]);
-      expect(expectedWongEmail).to.throw(Error, `You must be registered to make orders!`);
+      let action = () => PizzaShop.makeAnOrder('pesho@abv.bg', products.pizzas[0], products.drinks[0]);
+      assert.throw(action, Error, 'You must be registered to make orders!');
     })
 
     it('Should throw error on wrong pizza', () => {
-      PizzaShop.registerUser(validUser);
-      let expectedWongPizza = () => PizzaShop.makeAnOrder(validUser, undefined, products.drinks[0]);
-      expect(expectedWongPizza).to.throw(Error, "You must order at least 1 Pizza to finish the order.");
+      let user = PizzaShop.registerUser(validUser);
+      let action = () => PizzaShop.makeAnOrder(user.email, undefined, products.drinks[0]);
+      assert.throw(action, Error, "You must order at least 1 Pizza to finish the order.")
     })
 
-    it('Should push the order to PizzaShop.orders', () => {
-      PizzaShop.registerUser(validUser);
-      const expectedOrder = {
-        orderedPizza: products.pizzas[0],
-        orderedDrink: products.drinks[0],
-        email: validUser,
-        status: 'pending'
-      };
-      let id = PizzaShop.makeAnOrder(validUser, products.pizzas[0], products.drinks[0]);
-      let actualOrder = PizzaShop.orders[id];
-      expect(actualOrder).to.deep.equal(expectedOrder);
+    it('Should fail on ordering a wrong drink', () => {
+      let user = PizzaShop.registerUser(validUser);
+      let id = PizzaShop.makeAnOrder(user.email, products.pizzas[0], 3);
+      assert.isUndefined(user.orderHistory[id].orderedDrink);
+    })
+
+    it("Check order's format", () => {
+      let user = PizzaShop.registerUser(validUser);
+      let oid = PizzaShop.makeAnOrder(user.email, products.pizzas[0], products.drinks[0]);
+      let expectedOrder = { orderedPizza: products.pizzas[0], orderedDrink: products.drinks[0] };
+      assert.deepEqual(user.orderHistory[oid], expectedOrder);
     })
 
     it('Should return the a number', () => {
-      PizzaShop.registerUser(validUser);
-      let actual = PizzaShop.makeAnOrder(validUser, products.pizzas[0], products.drinks[0]);
+      let user = PizzaShop.registerUser(validUser);
+      let actual = PizzaShop.makeAnOrder(user.email, products.pizzas[0], products.drinks[0]);
       expect(isNaN(actual)).to.be.equal(false);
     })
   })
 
   describe("completeOrder() tests", function () {
     it('Should complete first order', () => {
-      PizzaShop.registerUser(validUser);
-      PizzaShop.makeAnOrder(validUser, products.pizzas[0], products.drinks[0])
-      let actualOrder = PizzaShop.completeOrder();
-      expect(actualOrder.status).to.deep.equal('completed');
+      let user = PizzaShop.registerUser(validUser);
+      PizzaShop.makeAnOrder(user.email, products.pizzas[0], products.drinks[0]);
+      assert.equal(PizzaShop.completeOrder().status, 'completed');
     })
   })
 
   describe("detailsAboutMyOrder({id}) tests", function () {
-    it('Should complete first order', () => {
-      PizzaShop.registerUser(validUser);
-      PizzaShop.makeAnOrder(validUser, products.pizzas[0], products.drinks[0])
-      let actualOrder = PizzaShop.completeOrder();
-      expect(actualOrder.status).to.deep.equal('completed');
+    it("Order id is invalid", () => {
+      assert.isUndefined(PizzaShop.detailsAboutMyOrder(10))
+    })
+    it('function returns the status of that order', () => {
+      let users = PizzaShop.registerUser(validUser);
+      let id = PizzaShop.makeAnOrder(users.email, products.pizzas[0], products.drinks[0]);
+      let orderDetails = PizzaShop.detailsAboutMyOrder(id);
+      assert(orderDetails == `Status of your order: ${PizzaShop.orders[id].status}`);
+    })
+  })
+
+  describe("doesTheUserExist({email}) tests", function () {
+    it("Order doesn't exist", () => {
+      let user = PizzaShop.registerUser(validUser);
+      assert.deepEqual(PizzaShop.doesTheUserExist(validUser), user)
     })
   })
 })
