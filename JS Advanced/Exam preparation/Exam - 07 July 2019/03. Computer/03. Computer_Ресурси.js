c11b81e6a948e2e6477ff1c3beef33655b93b023
razielvonchronos <razielvonchronos@gmail.com>
2019-10-25 16:40:53 +0300

    93/100 on 03. Computer, for Satan sake just kill me already

--- JS Advanced/Exam preparation/Exam - 07 July 2019/03. Computer/03. Computer_Ресурси.js
+++ JS Advanced/Exam preparation/Exam - 07 July 2019/03. Computer/03. Computer_Ресурси.js
@@ -1,4 +1,4 @@
-
+"use strict"
 
 class Computer {
     constructor(ramMemory, cpuGHz, hddMemory) {
@@ -14,84 +14,98 @@ class Computer {
     ram_usage() {
         return this.taskManager.reduce((acc, curr) => acc + curr.ramUsage, 0)
     }
-    used_space() {
-        return this.installedPrograms.reduce((acc, curr) => acc + curr.requiredSpace, 0);
+    findInstallation(name) {
+        return this.installedPrograms.find(x => x.name === name);
     }
-    free_space() {
-        return this.hddMemory - this.used_space();
+    findProccess(name) {
+        return this.taskManager.find(x => x.name === name);
     }
-    installAProgram(name, requiredSpace) {
+    handleInstall(name, requiredSpace) {
         let app = {};
-        if (this.free_space() < requiredSpace) {
-            throw new Error("There is not enough space on the hard drive");
-        }
         app.name = name;
-        app.requiredSpace = requiredSpace
-        this.installedPrograms.push(app)
+        app.requiredSpace = requiredSpace;
+        this.installedPrograms.push(app);
+        this.hddMemory -= app.requiredSpace;
         return app;
     }
-    isInstalled(name) {
-        return this.installedPrograms.findIndex(x => x.name === name);
+    handleOpen(app) {
+        let proccess = {};
+        proccess.name = app.name;
+        proccess.ramUsage = Number((app.requiredSpace / this.ramMemory) * 1.5);
+        proccess.cpuUsage = Number(((app.requiredSpace / this.cpuGHz) / 500) * 1.5);
+        this.taskManager.push(proccess);
+        if (this.ram_usage() >= 100 && this.cpu_usage() >= 100 || this.ram_usage() >= 100) {
+            throw new Error(`${app.name} caused out of memory exception`)
+        }
+        if (this.cpu_usage() >= 100) {
+            throw new Error(`${app.name} caused out of cpu exception`)
+        }
+        return proccess;
     }
-    isOppened(name) {
-        return this.taskManager.findIndex(x => x.name === name);
+    handleUninstall(app) {
+        let appId = this.installedPrograms.findIndex(x => x.name === app.name);
+        let proccessId = this.installedPrograms.findIndex(x => x.name === app.name);
+        if (proccessId) {
+            this.taskManager.splice(proccessId, 1);
+        }
+        this.hddMemory += app.requiredSpace;
+        return this.installedPrograms.splice(appId, 1);
+    }
+    installAProgram(name, requiredSpace) {
+        if (this.hddMemory < requiredSpace) {
+            throw new Error("There is not enough space on the hard drive");
+        }
+        return this.handleInstall(name, requiredSpace);
     }
     openAProgram(input) {
-        let i = this.isInstalled(input);
-        if (i >= 0) {
-            let app = this.installedPrograms[i];
-            let o = this.isOppened(app.name);
-            if (o >= 0) {
-                throw new Error(`The ${app.name} is already open`)
-            }
-            let proccess = {};
-            proccess.name = app.name;
-            proccess.ramUsage = Number((app.requiredSpace / this.ramMemory) * 1.5);
-            proccess.cpuUsage = Number(((app.requiredSpace / this.cpuGHz) / 500) * 1.5);
-            if (this.ram_usage() >= 100 && this.cpu_usage() > 100 || this.ram_usage() >= 100) {
-                throw new Error(`${app.name} caused out of memory exception`)
-            }
-            if (this.cpu_usage() >= 100) {
-                throw new Error(`${app.name} caused out of cpu exception`)
-            }
-            this.taskManager.push(proccess);
-            return proccess;
+        let app = this.findInstallation(input);
+        if (!app) {
+            throw new Error(`The ${input} is not recognized`);
         }
-        throw new Error(`The ${name} is not recognized`);
+
+        let proccess = this.findProccess(app);
+        if (proccess) {
+            throw new Error(`The ${app.name} is already open`)
+        }
+        proccess = this.handleOpen(app);
+        return proccess;
     }
     uninstallAProgram(input) {
-        let i = this.isInstalled(input)
-        if (i >= 0) {
-            this.installedPrograms.splice(i, 1);
-            return this.installedPrograms;
+        let app = this.findInstallation(input);
+        if (!app) {
+            throw new Error("Control panel is not responding");
         }
-        throw new Error("Control panel is not responding");
+        this.handleUninstall(app);
+        return this.installedPrograms;
     }
     taskManagerView() {
         let result = this.taskManager.reduce((msg, app) => {
             msg.push(`Name - ${app.name} | Usage - CPU: ${app.cpuUsage.toFixed(0)}%, RAM: ${app.ramUsage.toFixed(0)}%`)
             return msg;
         }, []);
-
         return result.length > 0 ? result.join("\n") : "All running smooth so far";
     }
 }
 
 function performance(computer) {
-    console.log(`RAM: ${computer.ram_usage()}`);
-    console.log(`CPU: ${computer.cpu_usage()}`);
-    console.log(`Hard Drive: ${computer.free_space()}/${computer.used_space()}`);
+    console.log(`Hard Drive: ${computer.hddMemory} CPU: ${computer.cpu_usage()}%, RAM: ${computer.ram_usage()}%`);
 }
-// Zero Test - 
-let computer = new Computer(13, 9.5, 250000);
 
-// act
+let computer = new Computer(4096, 7.5, 250000);
+
 computer.installAProgram('Word', 7300);
 computer.installAProgram('Excel', 10240);
 computer.installAProgram('PowerPoint', 12288);
-computer.uninstallAProgram('Word');
 computer.installAProgram('Solitare', 1500);
 
+computer.openAProgram('Word');
 computer.openAProgram('Excel');
+computer.openAProgram('PowerPoint');
 computer.openAProgram('Solitare');
-performance(computer)
+
+computer.uninstallAProgram('Word');
+computer.installAProgram('Word', 165970);
+computer.openAProgram('Word');
+console.log(computer);
+performance(computer);
+console.log(computer.taskManagerView());
