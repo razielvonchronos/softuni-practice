5b5bab450a6aaba20f2ce30bf41b31b325b5acdc
razielvonchronos <razielvonchronos@gmail.com>
2019-11-05 16:53:32 +0200

    Exercise: This is done, all exercises are 100/100

--- JS Applications/01. JS-Applications-This-Exercise/05. Next Article/next-article.js
+++ JS Applications/01. JS-Applications-This-Exercise/05. Next Article/next-article.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let i = 0;
+    let content = document.getElementById('content');
+    return function () {
+        if (articles.length - 1 >= i) {
+            let article = document.createElement('article');
+            article.textContent += articles[i];
+            content.appendChild(article);
+            i++
+        }
+    }
 }
