38e25e7539042e1f90587edb934c2f47731fa850
razielvonchronos <razielvonchronos@gmail.com>
2019-11-16 18:10:27 +0200

    Will continue later

--- JS Applications/04. Rest services and AJAX-Exercise/01.BUS-STOP/app.js
+++ JS Applications/04. Rest services and AJAX-Exercise/01.BUS-STOP/app.js
@@ -1,3 +1,18 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    let stopId = document.querySelector('#stopId');
+    let result_name = document.querySelector('#stopName');
+    let result_ul = document.querySelector('#buses');
+    try {
+        const response = await fetch(`https://judgetests.firebaseio.com/businfo/${stopId.value.trim()}.json`);
+        const data = await response.json();
+        result_name.textContent = data.name
+        result_ul.textContent = '';
+        Object.keys(data.buses).map((x) => {
+            result_ul.innerHTML += `<li>Bus ${x} arrives in ${data.buses[x]} minutes</li>`
+        })
+    }
+    catch (error) {
+        result_name.textContent = 'Error';
+        result_ul.textContent = '';
+    }
 }
--- JS Applications/04. Rest services and AJAX-Exercise/02.BUS-SCHEDULE/app.js
+++ JS Applications/04. Rest services and AJAX-Exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,33 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stopId = {
+        name: '',
+        next: 'depot'
+    };
+
+    let busStop = document.querySelector('#info span');
+    let buttons = document.querySelectorAll('#depart, #arrive');
+
+    function buttonsToggle() {
+        buttons.forEach(x => x.disabled = !x.disabled);
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function findStop() {
+        let response = await fetch(`https://judgetests.firebaseio.com/schedule/${stopId.next}.json`);
+        let data = await response.json();
+        return data;
+
+    }
+
+    async function depart() {
+        stopId = await findStop();
+        busStop.textContent = `Next Stop is ${stopId.name}`;
+        buttonsToggle();
+    }
+
+    async function arrive() {
+        busStop.textContent = `Arriving at ${stopId.name}`;
+        buttonsToggle();
     }
 
     return {
